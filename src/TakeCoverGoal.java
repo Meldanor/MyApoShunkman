80fcd4c7605345e3d2661bfe1fdc345035799dbf
Meldanor <Bhaals@gmx.de>
2012-04-22 01:00:37 +0200

    Implement first result for a TakeCoverGoal which is called when a bomb has been planted
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/TakeCoverGoal.java
@@ -0,0 +1,115 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+import apoSkunkman.ai.ApoSkunkmanAIConstants;
+import apoSkunkman.ai.ApoSkunkmanAILevel;
+import apoSkunkman.ai.ApoSkunkmanAILevelSkunkman;
+
+/*
+ * Copyright (C) 2012 Kilian Gaertner
+ * 
+ * Dieser Quelltext ist Open Source und kann von jedem verwendet werden, der 
+ * folgende Bedingung einhält:
+ * Jeder, der den Quelltext, ob in Teilen oder komplett,nutzt, muss dem Inhabenden
+ * des Copyrights eine Pizza spendieren, sollte derjenige dem Inhabenden des Copyrights
+ * begegnen.
+ */
+
+public class TakeCoverGoal extends Goal {
+
+    private final MeldanorPlayer player;
+
+    private Point cover;
+
+    private LinkedList<Node> path;
+
+    public TakeCoverGoal(MeldanorPlayer player, ApoSkunkmanAILevel apoLevel, ApoSkunkmanAILevelSkunkman bomb) {
+        this.player = player;
+        findCover(apoLevel, bomb);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return player.getPosition().equals(cover);
+    }
+
+    @Override
+    public void process() {
+        player.moveTo(path);
+
+    }
+    @Override
+    public GoalPriority getPriority() {
+        return GoalPriority.CRITICAL;
+    }
+
+    // SEARCHING FOR COVER AND CALCULATE A PATH TO IT
+    private void findCover(ApoSkunkmanAILevel apoLevel, ApoSkunkmanAILevelSkunkman bomb) {
+        Point start = player.getPosition();
+        Point pBomb = new Point((int) bomb.getX(), (int) bomb.getY());
+        int radius = bomb.getSkunkWidth() + 1;
+        byte[][] byteLevel = apoLevel.getLevelAsByte();
+
+        // CHECK POSSIBILITES
+        // @formatter:off
+        Point[] possi = {
+                getPoint(start.x + radius,  start.y,            byteLevel),
+                getPoint(start.x - radius,  start.y,            byteLevel),
+                getPoint(start.x ,          start.y + radius,   byteLevel),
+                getPoint(start.x ,          start.y - radius,   byteLevel),
+        };
+        // @formatter:on
+
+        List<LinkedList<Node>> paths = new ArrayList<LinkedList<Node>>(4);
+        for (Point pos : possi) {
+            if (pos == null) {
+                path = player.findWay(cover, apoLevel);
+                if (path != null) {
+                    paths.add(path);
+                    path = null;
+                }
+            }
+        }
+
+        // DIDN'T FIND A COVER DIRECTLY
+        if (paths.isEmpty())
+            ;
+        // TODO: SEARCH FOR COVERES THEIR AREN'T ON THE AXIS
+        // TODO: THINK LIKE A JUMPER
+
+        else {
+            // FIND THE PATH WITH THE SHORTEST WAY
+            path = Collections.min(paths, new Comparator<LinkedList<Node>>() {
+                @Override
+                public int compare(LinkedList<Node> o1, LinkedList<Node> o2) {
+                    return o1.size() - o2.size();
+                }
+            });
+        }
+
+    }
+    private Point getPoint(int x, int y, byte[][] byteLevel) {
+        // IS INSIDE THE FIELD AND NOT A STONE
+        if (y < byteLevel.length && y >= 0 && x < byteLevel[y].length && x >= 0 && byteLevel[y][x] != ApoSkunkmanAIConstants.LEVEL_STONE && byteLevel[y][x] != ApoSkunkmanAIConstants.LEVEL_BUSH)
+            return new Point(x, y);
+        // IS OUTSIDE
+        else
+            return null;
+    }
+//
+//    private List<Point> getNeighbors(Point p, ApoSkunkmanAILevel apoLevel) {
+//        byte[][] byteLevel = apoLevel.getLevelAsByte();
+//        List<Point> neighbors = new LinkedList<Point>();
+//
+//        // CHECK POINTS IN ROW OUTSIDE THE BOMB RADIUS
+//        // @formatter:off
+//        int bombRadius = 
+//        // @formatter:on
+//        return neighbors;
+//
+//    }
+}
