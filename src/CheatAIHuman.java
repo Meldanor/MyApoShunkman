0769724fcd89123de32c137890ed865b372b6c11
Meldanor <Bhaals@gmx.de>
2012-05-26 17:00:51 +0200

    This is buggy and must be reviewed ASAP! Implement a check whether a bomb can hit myself or not After one teleport the player have some problems
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/CheatAIHuman.java
+++ src/CheatAIHuman.java
@@ -1,11 +1,16 @@
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.lang.reflect.Field;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
+import apoSkunkman.ApoSkunkmanConstants;
+import apoSkunkman.ai.ApoSkunkmanAIConstants;
 import apoSkunkman.ai.ApoSkunkmanAIEnemy;
 import apoSkunkman.ai.ApoSkunkmanAILevel;
+import apoSkunkman.ai.ApoSkunkmanAILevelSkunkman;
 import apoSkunkman.ai.ApoSkunkmanAIPlayer;
 import apoSkunkman.entity.ApoSkunkmanPlayer;
 
@@ -22,6 +27,8 @@ import apoSkunkman.entity.ApoSkunkmanPlayer;
 public class CheatAIHuman implements Tickable, Initiationable {
 
     private boolean isInit = false;
+
+    private final static Random RAND = new Random();
 
     private ApoSkunkmanAILevel apoLevel;
     private ApoSkunkmanAIPlayer apoPlayer;
@@ -98,7 +105,110 @@ public class CheatAIHuman implements Tickable, Initiationable {
 
     }
 
+    private long checkPlayerTimer = 4000L;
+
     private void handleLevel(long delta) {
+        try {
+
+            checkMySelf();
+
+            if ((checkPlayerTimer -= delta) <= 0)
+                checkPlayer();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // IS THERE A BOMB WHICH CAN HIT MYSELF?
+    private void checkMySelf() throws Exception {
+
+        byte[][] byteLevel = apoLevel.getLevelAsByte();
+
+        Point myPosition = new Point((int) apoPlayer.getX(), (int) apoPlayer.getX());
+
+        for (int y = 0; y < byteLevel.length; ++y) {
+            for (int x = 0; x < byteLevel[y].length; ++x) {
+                if (byteLevel[y][x] == ApoSkunkmanAIConstants.LEVEL_SKUNKMAN) {
+                    ApoSkunkmanAILevelSkunkman skunk = apoLevel.getSkunkman(y, x);
+                    // IN MY ROW / COLUMN AND CAN HIT ME
+                    if (bombCanHit(myPosition, skunk, byteLevel))
+                        teleportRandom();
+                }
+            }
+        }
+
+    }
+
+    private boolean bombCanHit(Point playerPoint, ApoSkunkmanAILevelSkunkman bomb, byte[][] byteLevel) {
+
+        Point bombPoint = new Point((int) bomb.getX(), (int) bomb.getY());
+        // I'M STANDING ON THE BOMB
+        if (bombPoint.equals(playerPoint))
+            return true;
+
+        Point diff = new Point(bombPoint.x - playerPoint.x, bombPoint.y - playerPoint.y);
+
+        // BOMB IS NOT IN THE ROW OF PLAYER
+        // OR
+        // BOMB RADIUS TOO LOW
+        
+        // TODO : Nach einmal wegteleportieren kommen seltsame ergebnisse raus
+        if ((diff.x != 0 && diff.y != 0) || playerPoint.distance(bombPoint) > bomb.getSkunkWidth()) {
+            System.out.println(diff + "  " + playerPoint.distance(bombPoint) + " " + bomb.getSkunkWidth());
+            return false;
+
+        }
+
+        diff.x = (int) Math.signum(diff.x);
+        diff.y = (int) Math.signum(diff.y);
+
+        // CHECK WHETHER THERE IS A BARRIER BETWEEN BOMB AND PLAYER
+        if (diff.x != 0) {
+            for (int x = bombPoint.x + diff.x; x < playerPoint.x; x += diff.x) {
+                if (isBarrier(x, bombPoint.y, byteLevel))
+                    return false;
+            }
+        } else {
+            for (int y = bombPoint.y + diff.y; y < playerPoint.y; y += diff.y) {
+                if (isBarrier(bombPoint.x, y, byteLevel))
+                    return false;
+            }
+        }
+
+        // THERE WAS NO BARIER BETWEEN PLAYER AND BOMB
+        return true;
+    }
+    private boolean isBarrier(int x, int y, byte[][] byteLevel) {
+        return byteLevel[y][x] == ApoSkunkmanAIConstants.LEVEL_BUSH || byteLevel[y][x] == ApoSkunkmanAIConstants.LEVEL_STONE;
+    }
+
+    private void teleportRandom() throws Exception {
+        ApoSkunkmanPlayer player = (ApoSkunkmanPlayer) apoPlayerField.get(apoPlayer);
+
+        int x = 0;
+        int y = 0;
+        Point bombPoint = null;
+
+        // SEARCH FOR A TILE TO TELEPORT TO
+        do {
+            x = RAND.nextInt(ApoSkunkmanConstants.LEVEL_WIDTH - 1) + 1;
+            y = RAND.nextInt(ApoSkunkmanConstants.LEVEL_HEIGHT - 1) + 1;
+            bombPoint = new Point(x, y);
+        } while (!isFree(bombPoint));
+        player.setX(bombPoint.x * ApoSkunkmanConstants.TILE_SIZE);
+        player.setY(bombPoint.y * ApoSkunkmanConstants.TILE_SIZE);
+    }
+
+    // RETURNS TRUE WHEN ON THE FIELD IS NOTHING(NOR A GOODIE)
+    private boolean isFree(Point p) {
+        return apoLevel.getLevelAsByte()[p.y][p.x] == ApoSkunkmanAIConstants.LEVEL_FREE;
+    }
+
+    private void checkPlayer() throws Exception {
+
+        checkPlayerTimer = 4000L;
 
     }
 
