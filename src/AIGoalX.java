61aeeabed7c1cb91ebd3f13f57ca070a782c25a9
Meldanor <Bhaals@gmx.de>
2012-05-29 17:13:15 +0200

    Seperate the GoalX Logic from the complete AI and move them into a own class. AI Manager now split the AI depending on their tasks (see CheatAI model)
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/AIGoalX.java
@@ -0,0 +1,183 @@
+import java.awt.Point;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import apoSkunkman.ai.ApoSkunkmanAIConstants;
+import apoSkunkman.ai.ApoSkunkmanAILevel;
+import apoSkunkman.ai.ApoSkunkmanAILevelSkunkman;
+import apoSkunkman.ai.ApoSkunkmanAIPlayer;
+
+/*
+ * Copyright (C) 2012 Kilian Gaertner
+ * 
+ * Dieser Quelltext ist Open Source und kann von jedem verwendet werden, der 
+ * folgende Bedingung einhält:
+ * Jeder, der den Quelltext, ob in Teilen oder komplett,nutzt, muss dem Inhabenden
+ * des Copyrights eine Pizza spendieren, sollte derjenige dem Inhabenden des Copyrights
+ * begegnen.
+ */
+
+public class AIGoalX implements Tickable, Initiationable {
+
+    private boolean isInit = false;
+
+    private MeldanorPlayer melPlayer;
+    private ApoSkunkmanAILevel apoLevel;
+
+    // GOALS TO ACHIEVE
+    private Queue<Goal> goals;
+
+    // CURRENT GOAL TO ACHIEVE
+    private Goal currentGoal;
+
+    // GOAL TO TAKE COVER (AFTER BOMB HAS BEEN FOUND)
+    private Goal coverGoal;
+
+    public AIGoalX(MeldanorPlayer melPlayer, ApoSkunkmanAIPlayer apoPlayer, ApoSkunkmanAILevel apoLevel) {
+        this.melPlayer = melPlayer;
+        init(apoPlayer, apoLevel);
+    }
+
+    @Override
+    public void init(ApoSkunkmanAIPlayer apoPlayer, ApoSkunkmanAILevel apoLevel) {
+
+        this.apoLevel = apoLevel;
+        // CALCULATE THE WAY TO THE GOAL - IGNORE BUSHES
+        // CREATE SUB GOALS TO BOMB THE BUSHES
+        searchPathToGoal();
+        isInit = true;
+    }
+
+    private void searchPathToGoal() {
+        goals = new LinkedList<Goal>();
+        // FIND WAY TO GOAL WITH BUSHES
+        LinkedList<Node> path = melPlayer.findWay(apoLevel.getGoalXPoint(), apoLevel, false);
+        byte[][] byteLevel = apoLevel.getLevelAsByte();
+
+        // LOOK AT THE PATH AND FILTER ALL BOMB SPOTS
+        Iterator<Node> iter = path.iterator();
+        Node cur = null;
+        Node prev = null;
+        while (iter.hasNext()) {
+            prev = cur;
+            cur = iter.next();
+            // THERE IS A BUSH
+            if (byteLevel[cur.y][cur.x] == ApoSkunkmanAIConstants.LEVEL_BUSH) {
+                // CREATE GOALS WITHOUT THE PATH - CALCULATE THEM WHEN IT IS
+                // NEEDED
+                if (prev == null)
+                    goals.add(new PlantBombGoal(cur.getLocation(), melPlayer));
+                else
+                    goals.add(new PlantBombGoal(prev.getLocation(), melPlayer));
+            }
+        }
+        goals.add(new WalkGoal(apoLevel.getGoalXPoint(), melPlayer));
+
+        // PROCESS FIRST GOAL
+        currentGoal = goals.poll();
+        // CALCULATE FIRST
+        ((WalkGoal) currentGoal).calculateWay(apoLevel);
+    }
+
+    @Override
+    public boolean isInit() {
+        return isInit;
+    }
+
+    @Override
+    public void tick(ApoSkunkmanAIPlayer apoPlayer, ApoSkunkmanAILevel apoLevel) {
+        this.apoLevel = apoLevel;
+        melPlayer.update(apoPlayer, apoLevel);
+        handleLevel();
+    }
+
+    private void handleLevel() {
+
+        // CHECK IF BOMB CAN KILL PLAYER
+        if (coverGoal == null)
+            this.lookForBombs();
+
+        // HAVE TO TAKE COVER GOAL?
+        if (coverGoal != null) {
+            if (!coverGoal.isFinished()) {
+                coverGoal.process();
+                return;
+            } else {
+                coverGoal = null;
+                // recalculate way
+                ((WalkGoal) currentGoal).calculateWay(apoLevel);
+            }
+        }
+
+        if (currentGoal.isFinished() || currentGoal.isCancelled()) {
+            if (!goals.isEmpty()) {
+                currentGoal = goals.poll();
+                ((WalkGoal) currentGoal).calculateWay(apoLevel);
+            } else {
+                System.out.println("Some error >.<");
+            }
+
+        }
+        currentGoal.process();
+
+    }
+
+    // CHECK CURRENT LEVEL FOR BOMBS
+    private void lookForBombs() {
+
+        byte[][] byteLevel = apoLevel.getLevelAsByte();
+
+        ApoSkunkmanAILevelSkunkman bomb = null;
+        LinkedList<TakeCoverGoal> bombs = new LinkedList<TakeCoverGoal>();
+
+        // ITERATE OVER ALL POINTS
+        for (int y = 0; y < byteLevel.length; ++y) {
+            for (int x = 0; x < byteLevel[y].length; ++x) {
+                // THERE IS A BOMB
+                if (byteLevel[y][x] == ApoSkunkmanAIConstants.LEVEL_SKUNKMAN) {
+                    bomb = apoLevel.getSkunkman(y, x);
+                    // BOMB CAN HIT PLAYER -> SEARCH AND TAKE COVER
+                    if (canHitPlayer(bomb))
+                        bombs.add(new TakeCoverGoal(melPlayer, apoLevel, bomb));
+                }
+            }
+        }
+
+        // LOOK FOR THE MOST RELEVANT GOAL
+        if (bombs.size() > 1) {
+            // FIND OUT WHICH BOMB IS THE NEAREST
+            // THE SHORTEST BOMB IS THE BOMB WITH HIGH PRIORITY
+            double distance = Double.MAX_VALUE;
+            double curDistance = 0.0;
+            Point bombSpot = null;
+            Point playerPos = melPlayer.getPosition();
+
+            for (TakeCoverGoal goal : bombs) {
+                bombSpot = goal.getBombPosition();
+                curDistance = Math.abs(playerPos.distance(bombSpot));
+                // IS CURRENT BOMB IS NEARER THEN REFERENCE?
+                if (curDistance < distance) {
+                    distance = curDistance;
+                    this.coverGoal = goal;
+                }
+            }
+
+        } else if (bombs.size() == 1)
+            this.coverGoal = bombs.getFirst();
+        else
+            this.coverGoal = null;
+    }
+
+    private boolean canHitPlayer(ApoSkunkmanAILevelSkunkman bomb) {
+        float xDiff = Math.abs(bomb.getX() - melPlayer.apoPlayer.getX());
+        float yDiff = Math.abs(bomb.getY() - melPlayer.apoPlayer.getY());
+        int radius = bomb.getSkunkWidth();
+
+        // PLAYER IS ON THE SAME LINE AS THE BOMB
+        // AND IN THE BOMG RADIUS
+        return ((xDiff == 0 || yDiff == 0) && (xDiff <= radius || yDiff <= radius));
+
+    }
+
+}
--- src/AIManager.java
+++ src/AIManager.java
@@ -1,11 +1,5 @@
-import java.awt.Point;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-
 import apoSkunkman.ai.ApoSkunkmanAIConstants;
 import apoSkunkman.ai.ApoSkunkmanAILevel;
-import apoSkunkman.ai.ApoSkunkmanAILevelSkunkman;
 import apoSkunkman.ai.ApoSkunkmanAIPlayer;
 
 /*
@@ -18,184 +12,46 @@ import apoSkunkman.ai.ApoSkunkmanAIPlayer;
  * begegnen.
  */
 
-public class AIManager implements Updateable, Tickable {
+public class AIManager implements Tickable, Initiationable {
 
     // KI MANAGER HAS ALL NECESSARY VALUES?
-    boolean isInitialized = false;
+    private boolean isInit = false;
 
     // WRAPPER FOR APO PLAYER CLASS
-    private MeldanorPlayer player;
+    private MeldanorPlayer melPlayer;
 
-    private ApoSkunkmanAILevel apoLevel;
-
-    private int levelType;
-
-    // CURRENT GOAL TO ACHIEVE
-    private Goal currentGoal;
-
-    // GOAL TO TAKE COVER (AFTER BOMB HAS BEEN FOUND)
-    private Goal coverGoal;
-
-    // GOALS FOR THE GOAL TYPE LEVEL
-    private Queue<Goal> goalsForWalkLevel;
+    private Tickable ai;
 
     public AIManager() {
         // EMPTY CONSTRUCTOR
     }
 
-    private void handleGoalLevel() {
-        goalsForWalkLevel = new LinkedList<Goal>();
-        // FIND WAY TO GOAL WITH BUSHES
-        LinkedList<Node> path = player.findWay(apoLevel.getGoalXPoint(), apoLevel, false);
-        byte[][] byteLevel = apoLevel.getLevelAsByte();
+    @Override
+    public void init(ApoSkunkmanAIPlayer apoPlayer, ApoSkunkmanAILevel apoLevel) {
 
-        // LOOK AT THE PATH AND FILTER ALL BOMB SPOTS
-        Iterator<Node> iter = path.iterator();
-        Node cur = null;
-        Node prev = null;
-        while (iter.hasNext()) {
-            prev = cur;
-            cur = iter.next();
-            // THERE IS A BUSH
-            if (byteLevel[cur.y][cur.x] == ApoSkunkmanAIConstants.LEVEL_BUSH) {
-                // CREATE GOALS WITHOUT THE PATH - CALCULATE THEM WHEN IT IS
-                // NEEDED
-                if (prev == null)
-                    goalsForWalkLevel.add(new PlantBombGoal(cur.getLocation(), player));
-                else
-                    goalsForWalkLevel.add(new PlantBombGoal(prev.getLocation(), player));
-            }
-        }
-        goalsForWalkLevel.add(new WalkGoal(apoLevel.getGoalXPoint(), player));
+        // CREATE WRAPPER
+        melPlayer = new MeldanorPlayer(apoPlayer, apoLevel);
 
-        // PROCESS FIRST GOAL
-        currentGoal = goalsForWalkLevel.poll();
-        // CALCULATE FIRST
-        ((WalkGoal) currentGoal).calculateWay(apoLevel);
-    }
-
-    // CHECK CURRENT LEVEL FOR BOMBS
-    private void lookForBombs() {
-
-        byte[][] byteLevel = apoLevel.getLevelAsByte();
-
-        ApoSkunkmanAILevelSkunkman bomb = null;
-        LinkedList<TakeCoverGoal> bombs = new LinkedList<TakeCoverGoal>();
-
-        // ITERATE OVER ALL POINTS
-        for (int y = 0; y < byteLevel.length; ++y) {
-            for (int x = 0; x < byteLevel[y].length; ++x) {
-                // THERE IS A BOMB
-                if (byteLevel[y][x] == ApoSkunkmanAIConstants.LEVEL_SKUNKMAN) {
-                    bomb = apoLevel.getSkunkman(y, x);
-                    // BOMB CAN HIT PLAYER -> SEARCH AND TAKE COVER
-                    if (canHitPlayer(bomb))
-                        bombs.add(new TakeCoverGoal(player, apoLevel, bomb));
-
-                }
-            }
+        if (apoLevel.getType() == ApoSkunkmanAIConstants.LEVEL_TYPE_GOAL_X)
+            ai = new AIGoalX(melPlayer, apoPlayer, apoLevel);
+        else {
+            // TODO: Implement second AI for handeling fighting the bots
         }
 
-        // LOOK FOR THE MOST RELEVANT GOAL
-        if (bombs.size() > 1) {
-            // FIND OUT WHICH BOMB IS THE NEAREST
-            // THE SHORTEST BOMB IS THE BOMB WITH HIGH PRIORITY
-            double distance = Double.MAX_VALUE;
-            double curDistance = 0.0;
-            Point bombSpot = null;
-            Point playerPos = player.getPosition();
+        isInit = true;
 
-            for (TakeCoverGoal goal : bombs) {
-                bombSpot = goal.getBombPosition();
-                curDistance = Math.abs(playerPos.distance(bombSpot));
-                // IS CURRENT BOMB IS NEARER THEN REFERENCE?
-                if (curDistance < distance) {
-                    distance = curDistance;
-                    this.coverGoal = goal;
-                }
-            }
-
-        } else if (bombs.size() == 1)
-            this.coverGoal = bombs.getFirst();
-        else
-            this.coverGoal = null;
     }
 
-    private boolean canHitPlayer(ApoSkunkmanAILevelSkunkman bomb) {
-        float xDiff = Math.abs(bomb.getX() - player.apoPlayer.getX());
-        float yDiff = Math.abs(bomb.getY() - player.apoPlayer.getY());
-        int radius = bomb.getSkunkWidth();
-
-        // PLAYER IS ON THE SAME LINE AS THE BOMB
-        // AND IN THE BOMG RADIUS
-        return ((xDiff == 0 || yDiff == 0) && (xDiff <= radius || yDiff <= radius));
-
+    @Override
+    public boolean isInit() {
+        return isInit;
     }
 
     @Override
     public void tick(ApoSkunkmanAIPlayer apoPlayer, ApoSkunkmanAILevel apoLevel) {
-
-        // UPDATE VALUES
-        this.update(apoPlayer, apoLevel);
-
-        // CHECK IF BOMB CAN KILL PLAYER
-        if (coverGoal == null)
-            this.lookForBombs();
-
-        // HAVE TO TAKE COVER GOAL?
-        if (coverGoal != null) {
-            if (!coverGoal.isFinished()) {
-                coverGoal.process();
-                return;
-            } else {
-                coverGoal = null;
-                // recalculate way
-                ((WalkGoal) currentGoal).calculateWay(apoLevel);
-            }
-        }
-
-        if (currentGoal.isFinished() || currentGoal.isCancelled()) {
-            if (levelType == ApoSkunkmanAIConstants.LEVEL_TYPE_GOAL_X) {
-                if (!goalsForWalkLevel.isEmpty()) {
-                    currentGoal = goalsForWalkLevel.poll();
-                    ((WalkGoal) currentGoal).calculateWay(apoLevel);
-                    // UGLY FIX!
-                    if (currentGoal.isCancelled()) {
-                        player.goBack();
-                        ((WalkGoal) currentGoal).calculateWay(apoLevel);
-                    }
-                } else {
-                    System.out.println("Kalkuliere erneut");
-                    
-                    handleGoalLevel();
-                    return;
-
-                }
-            } else {
-                System.out.println("Muss was neues finden :/");
-                // TODO: FIND A NEW ONE
-            }
-
-        }
-        currentGoal.process();
+        if (isInit)
+            ai.tick(apoPlayer, apoLevel);
+        else
+            init(apoPlayer, apoLevel);
     }
-    // UPDATING VALUES
-    @Override
-    public void update(ApoSkunkmanAIPlayer apoPlayer, ApoSkunkmanAILevel apoLevel) {
-
-        if (isInitialized) {
-            this.apoLevel = apoLevel;
-            this.player.update(apoPlayer, apoLevel);
-        } else {
-            player = new MeldanorPlayer(apoPlayer, apoLevel);
-            this.apoLevel = apoLevel;
-            levelType = apoLevel.getType();
-            if (levelType == ApoSkunkmanAIConstants.LEVEL_TYPE_GOAL_X) {
-                handleGoalLevel();
-
-            }
-            isInitialized = true;
-        }
-    }
-
 }
